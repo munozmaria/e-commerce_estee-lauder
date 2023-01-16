9eab050e11ecf71da476aeb2d6aa83f08e54c4ce
Maria Munoz Rodriguez <1593maria@gmail.com>
2023-01-16 18:10:49 +0100

    left/right buttons

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { BsChevronLeft } from "react-icons/bs"
 
 import './App.css';
 
+
 function Header() {
   return (
 		<>
@@ -45,39 +46,95 @@ function Header() {
 	)
 }
 
+function Lightbox({products, slideIndex, nextSlide, previousSlide, index}) {
+  return (
+		<>
+			<article className="bg-black bg-opacity-75 absolute top-0 left-0 right-0 bottom-0 z-50">
+        <div>
+          {products.map((item, index) => (
+          <div
+								key={index}
+								className={slideIndex === index + 1 ? "relative" : "hidden"}
+							>
+								<img
+									src={item.mainImage}
+									alt=""
+									className="lg:rounded-2xl"
+								></img>
+								<ul >
+									<li>
+										<button
+											onClick={previousSlide}
+											className="bg-white rounded-full font-bold p-4 shadow absolute left-4 top-1/2 -translate-y-1/2 "
+										>
+											<BsChevronLeft />
+										</button>
+									</li>
+									<li>
+										<button
+											onClick={nextSlide}
+											className="bg-white rounded-full font-bold  p-4 shadow absolute right-4 top-1/2 -translate-y-1/2"
+										>
+											<BsChevronRight />
+										</button>
+									</li>
+								</ul>
+							</div>
+          ))}
+        </div>
+			</article>
+		</>
+	)
+}
+
 function App() {
 
   const [products] = useState(data)
-
   const [value, setValue] = useState(0)
-  const { mainImage } = products[value]
   const [amount, setAmount] = useState(0)
+  const [slideIndex, setSlideIndex] = useState(1)
+  
+  const { mainImage } = products[value]
+  const nextSlide = () => {
+    if (slideIndex !== products.length) {
+      setSlideIndex(slideIndex + 1)
+    } else if (slideIndex === products.length) {
+      setSlideIndex(1)
+    }
+  }
+  const previousSlide = () => {
+    if (setSlideIndex !== 1) {
+      setSlideIndex(slideIndex - 1)
+    } else if (slideIndex === 1) {
+      setSlideIndex(products.length)
+    }
+    }
 
   const handleMinus = () => {
     setAmount(amount - 1)
     if(amount <= 0) setAmount(0)
-}
+  }
   return (
 		<>
 			<Header></Header>
 
-			<section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 lg:mt-10">
+			<section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10  lg:place-items-center lg:py-20">
 				<article>
 					<div className="relative">
 						<img src={mainImage} alt="" className="w-full lg:rounded-2xl"></img>
 						<ul>
 							<li>
 								<button className="bg-white rounded-full font-bold p-4 shadow absolute left-4 top-1/2 -translate-y-1/2 ">
-									<BsChevronRight />
+									<BsChevronLeft />
 								</button>
 							</li>
 							<li>
 								<button className="bg-white rounded-full font-bold  p-4 shadow absolute right-4 top-1/2 -translate-y-1/2">
-									<BsChevronLeft />
+									<BsChevronRight />
 								</button>
 							</li>
 						</ul>
-					</div>
+					</div>{" "}
 					<ul className="hidden lg:flex items-center justify-start gap-5 flex-wrap  mt-5">
 						{products.map((item, index) => (
 							<li
@@ -100,7 +157,7 @@ function App() {
 						DOUBLE WEAR STAY-IN-PLACE MAKEUP SPF10 30ML - Fond de teint
 					</h1>
 
-					<div className="flex flex-wrap items-center justify-between">
+					<div className="flex flex-wrap items-center justify-between lg:flex-col lg:items-start lg:gap-2">
 						<ul className="flex items-center gap-4">
 							<li className="font-bold text-slate-900 text-2xl">23.5€</li>
 							<li className="bg-orange-100 py-1 px-2 text-orange-400 uppercase tracking-wide text-sm font-bold inline-block rounded shadow">
@@ -111,8 +168,8 @@ function App() {
 							<s>42.50€</s>
 						</p>
 					</div>
-					<div className="mt-10">
-						<ul className="flex items-center justify-between bg-slate-100 py-2 px-4 rounded shadow">
+					<div className="mt-10 lg:flex items-center justify-between gap-2">
+						<ul className="flex items-center justify-between bg-slate-100 py-2 px-4 rounded shadow lg:flex-1">
 							<li
 								onClick={handleMinus}
 								className="text-orange-600 cursor-pointer"
@@ -128,10 +185,11 @@ function App() {
 							</li>
 							<li></li>
 						</ul>
-
-						<button className="flex items-center justify-center gap-4 bg-orange-400 py-2 px-4 text-white font-bold rounded-lg shadow mt-4 w-full">
-							<AiOutlineShoppingCart /> Ajouter au panier
-						</button>
+						<div className="lg:flex-1">
+							<button className="flex items-center justify-center gap-4 bg-orange-400 py-2 px-4 text-white font-bold rounded-lg shadow mt-4 w-full lg:mt-0 hover:bg-orange-600 transition-all duration-200">
+								<AiOutlineShoppingCart /> Ajouter au panier
+							</button>
+						</div>
 					</div>
 				</article>
 			</section>
