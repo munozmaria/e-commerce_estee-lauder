c8be96802cf9bc466d225a746db0f8a1fcd5fd4e
Maria Munoz Rodriguez <1593maria@gmail.com>
2023-01-16 12:08:04 +0100

    pictures + header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
+import { data } from './data';
+import logo from "./images/estee-lauder-logo-vector.svg"
+import { BsFillPersonFill } from "react-icons/bs"
+import { AiOutlineShoppingCart } from "react-icons/ai"
+
 import './App.css';
 
-function App() {
+function Header() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<>
+			<header className="flex items-center justify-between p-8 border-b border-slate-400 max-w-6xl mx-auto">
+				<div className="flex items-center justify-start gap-4">
+					<img src={logo} className="w-20" alt=""></img>
+					<nav>
+						<ul className="flex items-center justify-start gap-4">
+							<li>Accueil</li>
+							<li>Shopping</li>
+							<li>Magasins</li>
+							<li>Mes favoris</li>
+							<li>Mon compte</li>
+						</ul>
+					</nav>
+				</div>
+				<div>
+					<ul>
+						<li className="flex items-center justify-start gap-4">
+							<button>
+								<AiOutlineShoppingCart />
+							</button>
+							<button>
+								<BsFillPersonFill />
+							</button>
+						</li>
+						<li></li>
+					</ul>
+				</div>
+			</header>
+		</>
+	)
+}
+
+function App() {
+
+  const [products] = useState(data)
+
+  const [value, setValue] = useState(0)
+  const{mainImage} = products[value]
+
+  return (
+		<>
+			<Header></Header>
+
+			<section className='max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10'>
+        <article>
+          <img src={mainImage} alt="" className='w-9/12 rounded-2xl'></img>
+          <ul className='flex items-center justify-start gap-5 flex-wrap cursor-pointer mt-5'>
+              {products.map((item, index) => (
+                
+                  <li key={item.id} onClick={() => setValue(index)}>
+                    <img src={item.thumbnail} alt="" className='w-20 rounded-xl'></img>
+                  </li>
+              ))}
+            </ul>
+        </article>
+        <article>
+          <h1>Lorem reorzoruzorz</h1>
+        </article>
+			</section>
+		</>
+	)
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,33 @@
+import image1 from "../src/images/product1.jpg"
+import image2 from "../src/images/pic2.jpg"
+import image3 from "../src/images/pic3.jpg"
+import image4 from "../src/images/pic4.jpg"
+import image1Thumb from "../src/images/product1-thumbnail.jpg"
+import image2Thumb from "../src/images/pic2-thumbnail.jpg"
+import image3Thumb from "../src/images/pic3-thumbnail.jpg"
+import image4Thumb from "../src/images/pic4-thumbnail.jpg"
+
+
+
+export const data = [
+	{
+		id: 1,
+		mainImage: image1,
+		thumbnail: image1Thumb,
+	},
+	{
+		id: 2,
+		mainImage: image2,
+		thumbnail: image2Thumb,
+	},
+	{
+		id: 3,
+		mainImage: image3,
+		thumbnail: image3Thumb,
+	},
+	{
+		id: 4,
+		mainImage: image4,
+		thumbnail: image4Thumb,
+	},
+]
