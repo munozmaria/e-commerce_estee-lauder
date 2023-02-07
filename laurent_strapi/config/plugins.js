fddfee3ec3a67f6ae645692a1f69260c33a75914
Maria Munoz Rodriguez <1593maria@gmail.com>
2023-02-07 12:16:32 +0100

    plugin

--- laurent_strapi/config/plugins.js
+++ laurent_strapi/config/plugins.js
@@ -2,12 +2,11 @@ module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
     config: {
-      deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
-      apiToken: "<vercel-api-token>",
-      appFilter: "your-app-name-on-vercel",
-      teamFilter: "your-team-id-on-vercel",
-      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+      teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
+      roles: ["strapi-super-admin"],
     },
   },
   upload: {
